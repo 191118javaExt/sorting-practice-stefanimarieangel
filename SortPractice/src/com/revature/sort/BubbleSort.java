445af99cfa6beac667a72669485da95aa46c728e
stefani <stefanimarieangel@gmail.com>
2019-12-09 17:05:25 -0500

    added sorting answer

--- SortPractice/src/com/revature/sort/BubbleSort.java
+++ SortPractice/src/com/revature/sort/BubbleSort.java
@@ -1,8 +1,24 @@
 package com.revature.sort;
 
 public class BubbleSort {
-	
+
 	public static void sort(int[] arr) {
-		// TODO Auto-generated method stub
+		int i, j, temp = 0;
+		/*
+		 * 
+		 */
+		for (i = 0; i < arr.length - 1; i++) {
+			for (j = 0; j < arr.length - 1 - i; j++) {
+				if (arr[j] > arr[j + 1]) {
+					temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+				}
+			}
+			// TODO Auto-generated method stub
+		}
+		
 	}
+	
+
 }
--- SortPractice/src/com/revature/sort/MergeSort.java
+++ SortPractice/src/com/revature/sort/MergeSort.java
@@ -3,10 +3,60 @@ package com.revature.sort;
 public class MergeSort {
 	
 	public static void sort(int[] arr) {
+		// Check if the lenght of the array is less than 2 in order to keep the sorting
+	    if(arr.length < 2) {
+	      return ;
+	    }
+	    int arraySize = arr.length;
+	    // Find the middle of the array
+	    int middle = arraySize / 2;
+	    
+	    
+	    // Create new Arrays one for left and right
+	    int[] leftArray = new int[middle];
+	    int[] rightArray = new int[arraySize - middle];
+	    
+	    //Populate each array
+	    for(int i = 0; i < middle; i++) {
+	      leftArray[i] = arr[i];
+	    }
+	    for(int j = middle ; j < arraySize; j++ ){
+	      rightArray[j - middle] = arr[j];
+	    }
+	    
+	    // Recursively sorting arrays
+	    sort(leftArray);
+	    sort(rightArray);
+	    
+	    // Merge and sort last two arrays into one
+	    merge(arr, leftArray, rightArray, middle, arraySize - middle);
+				
+			// TODO Auto-generated method stub
+		}
+		
+	
 		// TODO Auto-generated method stub
+	
+	
+	private static void merge(int[] arr, int[] left, int[] right, int leftSize, int rightSize) {
+	    // Creating basis iterators for the arrays
+	    int j = 0, i = 0, k = 0;
+	    // int leftSize = left.length;
+	    // int rightSize = right.length;
+	    
+	    while(i < leftSize && j < rightSize){
+	      	if(left[i] <= right[j]) {
+	          	arr[k++] = left[i++];
+	        } else {
+	            arr[k++] = right[j++];
+	        }
+	    }
+	    while(i < leftSize){
+	      arr[k++] = left[i++];
+	    }
+	    while(j < rightSize) {
+	      arr[k++] = right[j++];
+	    }
 	}
 	
-	private static void merge(int[] arr, int[] left, int[] right) {
-		// TODO Auto-generated method stub
-	}
 }
